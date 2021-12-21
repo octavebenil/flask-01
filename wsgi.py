b0e216032da0ea5f7d553ee897b7c4b92bb3d652
Octave benil <asetoben@gmail.com>
2021-12-21 14:35:15 +0300

    READ

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from flask import json
+from flask import json, abort
 app = Flask(__name__)
 
 PRODUCTS = {
     1: { 'id': 1, 'name': 'Skello' },
     2: { 'id': 2, 'name': 'Socialive.tv' },
+    3: { 'id': 3, 'name': 'Monde.fr' }
 }
 
 @app.route('/')
@@ -13,4 +14,11 @@ def hello():
 
 @app.route("/api/v1/products", methods=["GET"])
 def products():
-    return json.jsonify(PRODUCTS)    
+    return json.jsonify(PRODUCTS)
+
+@app.route("/api/v1/products/<int:id>", methods=["GET"])
+def product(id):
+    if id in PRODUCTS.keys():
+        return json.jsonify(PRODUCTS[id]), 200
+    else:
+        abort(404)
