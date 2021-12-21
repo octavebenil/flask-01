3e00f06a57adbb673c4664489e41ea8fc4aa02b1
Octave benil <asetoben@gmail.com>
2021-12-21 15:01:56 +0300

    Create one product

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
 from flask import Flask
-from flask import json, abort
+from flask import json, abort, request
+
+#auto incrementation
+import itertools
+
 app = Flask(__name__)
 
 PRODUCTS = {
@@ -17,8 +21,27 @@ def products():
     return json.jsonify(PRODUCTS)
 
 @app.route("/api/v1/products/<int:id>", methods=["GET"])
-def product(id):
+def readProduct(id):
     if id in PRODUCTS.keys():
         return json.jsonify(PRODUCTS[id]), 200
     else:
         abort(404)
+
+@app.route("/api/v1/products/<int:id>", methods=["DELETE"])
+def deleteProduct(id):
+    PRODUCTS.pop(id, None)
+    return None, 204 
+
+@app.route("/api/v1/products", methods=["POST"])
+def createProduct():
+    posted_product = request.get_json()
+
+    print(posted_product)
+
+    START_INDEX = len(PRODUCTS) + 1
+    IDENTIFIER_GENERATOR = itertools.count(START_INDEX)
+
+    new_id = next(IDENTIFIER_GENERATOR)
+    PRODUCTS[new_id] = {'id': new_id, 'name': posted_product["name"]}
+
+    return json.jsonify(PRODUCTS), 201                  
